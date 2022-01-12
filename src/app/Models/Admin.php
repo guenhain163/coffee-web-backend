7652ecd0e39a6f7655604ce280aaa198148ad731
guenhain163 <guenhain.163@gmail.com>
2022-01-12 11:17:46 +0700

    fix login admin

--- src/app/Models/Admin.php
+++ src/app/Models/Admin.php
@@ -8,8 +8,10 @@ use Illuminate\Notifications\Notifiable;
 
 class Admin extends Authenticatable
 {
-    //
     use Notifiable;
+
+    const STATUS_ACTIVE = 1;
+    const STATUS_BLOCKED = 0;
 
     protected $guard = 'admin';
 
--- src/resources/views/admin/auth/login.blade.php
+++ src/resources/views/admin/auth/login.blade.php
@@ -1,78 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Đăng nhập | Hategi</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="/admin-lte/plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="/admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="/admin-lte/dist/css/adminlte.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/admin-lte/plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="/admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="/admin-lte/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="/assets/css/login.css">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
-  <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+    <div class="login-logo">
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-card-body text-center">
+        <h1 class="text-center my-3 mb-lg-5">Đăng nhập</h1>
+        <form method="POST" action="{{ route('admin.login') }}" autocomplete="off">
+            @csrf
+            <div class="form-group">
+                <label for="email">Email <span>*</span></label>
+                <div class="input-group has-validation">
+                    <input type="text" class="form-control form-control-lg" name="email" id="email" value="{{ old('email') }}">
+                    @if($errors->has('email'))
+                        <div class="error">{{ $errors->first('email') }}</div>
+                    @endif
+                </div>
+            </div>
 
-      <form method="POST" action="{{ route('admin.login') }}">
-        @csrf
-        <div class="input-group mb-3">
-          <input type="email" name="email" class="form-control" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+            <div class="form-group mb-2">
+                <label for="password">Mật khẩu <span>*</span></label>
+                <div class="input-group has-validation">
+                    <input type="password" class="form-control form-control-lg" name="password" id="password">
+                    @if($errors->has('password'))
+                        <div class="error">{{ $errors->first('password') }}</div>
+                    @endif
+                </div>
             </div>
-           
-          </div>
-          @if($errors->has('email'))
-              <div class="error">{{ $errors->first('email') }}</div>
-            @endif
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-            
-          </div>
-          @if($errors->has('password'))
-              <div class="error">{{ $errors->first('password') }}</div>
-            @endif
-         
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-      <p class="mb-1">
-        <a href="forgot-password">I forgot my password</a>
-      </p>
+            <button type="submit" class="btn font-weight-bold">Đăng nhập</button>
+        </form>
     </div>
     <!-- /.login-card-body -->
-  </div>
 </div>
 <!-- /.login-box -->
 
